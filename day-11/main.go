71e7832fc465c73a250fa409bd4f9c5499b2ffe3
Casey Merkey <casey.merkey@gmail.com>
2024-12-11 19:47:22 -0500

    Better but still shitty

--- day-11/main.go
+++ day-11/main.go
@@ -5,13 +5,22 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"slices"
 	"strconv"
 	"strings"
 	"time"
 )
 
+type Node struct {
+	value      string
+	childCount int
+	left       *Node
+	right      *Node
+}
+
+var nodeMap = make(map[string]Node)
+
 func main() {
+
 	inputFile := "sample.txt"
 	if len(os.Args) > 1 && len(os.Args[1]) > 0 {
 		inputFile = os.Args[1]
@@ -29,6 +38,9 @@ func main() {
 		puzzle = strings.Fields(line)
 	}
 
+	node := Node{value: "0"}
+	nodeMap["0"] = node
+
 	var startTime = time.Now()
 	result := part1(puzzle)
 	fmt.Printf("Part 1: %d\n", result)
@@ -40,37 +52,97 @@ func main() {
 	fmt.Printf("Part 2: %d\n", result)
 	executionTime = float32(time.Since(startTime).Milliseconds()) / float32(1000)
 	fmt.Printf("Completed Part 2 in %f seconds\n", executionTime)
+
+	fmt.Println(result)
 }
 
 func part1(puzzle []string) int {
 
-	workingCopy := make([]string, 0)
-	workingCopy = append(workingCopy, puzzle...)
+	total := 0
+	for _, str := range puzzle {
+		exploded := expand(str, 25)
+		total += len(exploded)
+	}
 
-	for n := 0; n < 25; n++ {
-		for i := len(workingCopy) - 1; i >= 0; i-- {
-			if workingCopy[i] == "0" {
-				workingCopy[i] = "1"
-			} else if len(workingCopy[i])%2 == 0 {
-				n1, n2 := split(workingCopy[i])
-				workingCopy = slices.Insert(workingCopy, i, strconv.Itoa(n1))
-				workingCopy[i+1] = strconv.Itoa(n2)
+	return total
+}
 
-			} else {
-				n, _ := strconv.Atoi(workingCopy[i])
-				workingCopy[i] = strconv.Itoa(2024 * n)
+func part2(puzzle []string) int {
+
+	total := 0
+	for _, str := range puzzle {
+		exploded := expand(str, 75)
+		total += len(exploded)
+	}
+
+	return total
+}
+
+func expand(number string, iterations int) []string {
+
+	result := make([]string, 0)
+
+	node, _ := nodeMap[number]
+	if node.childCount == 0 {
+		if number == "0" {
+			value := "1"
+			left, found := nodeMap[value]
+			if !found {
+				left = Node{value: value}
+				nodeMap[value] = left
 			}
+			node.left = &left
+			node.childCount = 1
+
+		} else if len(number)%2 == 0 {
+			n1, n2 := split(number)
+			var left, right Node
+			var found bool
+			value := strconv.Itoa(n1)
+			left, found = nodeMap[value]
+			if !found {
+				left = Node{value: value}
+				nodeMap[value] = left
+			}
+			value = strconv.Itoa(n2)
+			right, found = nodeMap[value]
+			if !found {
+				right = Node{value: value}
+				nodeMap[value] = right
+			}
+			node.childCount = 2
+			node.left = &left
+			node.right = &right
+		} else {
+			n, _ := strconv.Atoi(number)
+			value := strconv.Itoa(2024 * n)
+			left, found := nodeMap[value]
+			if !found {
+				left = Node{value: value}
+				nodeMap[value] = left
+			}
+			node.left = &left
+			node.childCount = 1
 		}
 	}
-	return len(workingCopy)
+
+	if iterations > 1 {
+		result = append(result, expand(node.left.value, iterations-1)...)
+		if node.childCount > 1 {
+			result = append(result, expand(node.right.value, iterations-1)...)
+		}
+	} else {
+		result = append(result, node.left.value)
+		if node.childCount > 1 {
+			result = append(result, node.right.value)
+		}
+	}
+
+	return result
 }
 
 func split(number string) (int, int) {
 	n1, _ := strconv.Atoi(number[:len(number)/2])
 	n2, _ := strconv.Atoi(number[len(number)/2:])
 	return n1, n2
-}
-
-func part2(puzzle []string) int {
-	return 0
 }
