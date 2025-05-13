3a2655abf7bc8f67413efdbb0d367dbd67d73919
Casey Merkey <casey.merkey@gmail.com>
2025-05-13 15:20:02 -0400

    Another refactor to extract evaluation logic

--- day-17/main.go
+++ day-17/main.go
@@ -12,6 +12,84 @@ import (
 	"time"
 )
 
+type Computer struct {
+	a, b, c int64
+	program []int
+	pointer int
+	output  []int
+}
+
+func (c Computer) Run(A, B, C int64, program []int) []int {
+
+	c.a = A
+	c.b = B
+	c.c = C
+	c.program = program
+	c.output = make([]int, 0)
+	c.pointer = 0
+
+	for c.pointer = 0; c.pointer < len(c.program); {
+		c.doInstruction()
+	}
+	return c.output
+
+}
+
+func (c *Computer) doInstruction() {
+	opcode := c.program[c.pointer]
+	operand := c.program[c.pointer+1]
+
+	c.pointer += 2
+	switch opcode {
+	case 0:
+		numerator := c.a
+		denominator := math.Pow(2, float64(c.comboOperandValue(operand)))
+		c.a = numerator / int64(denominator)
+
+	case 1:
+		c.b = c.b ^ int64(operand)
+
+	case 2:
+		c.b = c.comboOperandValue(operand) % 8
+
+	case 3:
+		if c.a != 0 {
+			c.pointer = operand
+		}
+
+	case 4:
+		c.b = c.b ^ c.c
+
+	case 5:
+		c.output = append(c.output, int(c.comboOperandValue(operand)%8))
+
+	case 6:
+		numerator := c.a
+		denominator := math.Pow(2, float64(c.comboOperandValue(operand)))
+		c.b = numerator / int64(denominator)
+
+	case 7:
+		numerator := c.a
+		denominator := math.Pow(2, float64(c.comboOperandValue(operand)))
+		c.c = numerator / int64(denominator)
+	}
+}
+
+func (c *Computer) comboOperandValue(op int) int64 {
+
+	switch op {
+	case 4:
+		return c.a
+	case 5:
+		return c.b
+	case 6:
+		return c.c
+	case 7:
+		log.Fatal("Illegal Combo Operand")
+	}
+	return int64(op)
+}
+
 func main() {
 	inputFile := "sample.txt"
 	if len(os.Args) > 1 && len(os.Args[1]) > 0 {
@@ -60,77 +138,7 @@ func main() {
 
 func part1(registers map[string]int64, program []int) []int {
 
-	output := make([]int, 0)
-	for pointer := 0; pointer < len(program); {
-		opcode := program[pointer]
-		operand := program[pointer+1]
-
-		ptr, out := doInstruction(opcode, operand, registers)
-		if ptr >= 0 {
-			pointer = ptr
-		} else {
-			pointer += 2
-		}
-		if out >= 0 {
-			output = append(output, out)
-		}
-	}
+	computer := Computer{}
+	output := computer.Run(registers["A"], registers["B"], registers["C"], program)
 	return output
-}
-
-func comboOperandValue(op int, registers map[string]int64) int64 {
-
-	switch op {
-	case 4:
-		return registers["A"]
-	case 5:
-		return registers["B"]
-	case 6:
-		return registers["C"]
-	case 7:
-		log.Fatal("Illegal Combo Operand")
-	}
-	return int64(op)
-}
-
-func doInstruction(operator int, operand int, registers map[string]int64) (int, int) {
-
-	jump := -1
-	out := -1
-
-	switch operator {
-	case 0:
-		numerator := registers["A"]
-		denominator := math.Pow(2, float64(comboOperandValue(operand, registers)))
-		registers["A"] = numerator / int64(denominator)
-
-	case 1:
-		registers["B"] = registers["B"] ^ int64(operand)
-
-	case 2:
-		registers["B"] = comboOperandValue(operand, registers) % 8
-
-	case 3:
-		if registers["A"] != 0 {
-			jump = operand
-		}
-
-	case 4:
-		registers["B"] = registers["B"] ^ registers["C"]
-
-	case 5:
-		out = int(comboOperandValue(operand, registers) % 8)
-
-	case 6:
-		numerator := registers["A"]
-		denominator := math.Pow(2, float64(comboOperandValue(operand, registers)))
-		registers["B"] = numerator / int64(denominator)
-
-	case 7:
-		numerator := registers["A"]
-		denominator := math.Pow(2, float64(comboOperandValue(operand, registers)))
-		registers["C"] = numerator / int64(denominator)
-	}
-
-	return jump, out
 }
