247de67b22191184a4312cb418356217069d5247
Casey Merkey <casey.merkey@gmail.com>
2025-05-19 16:13:56 -0400

    Part 2

--- day-19/main.go
+++ day-19/main.go
@@ -42,13 +42,18 @@ func main() {
 
 	var startTime = time.Now()
 	result := part1(availableTowels, desiredPatterns)
-	resultString := strings.Trim(strings.Join(strings.Fields(fmt.Sprint(result)), ","), "[]")
-	fmt.Printf("Part 1: %s\n", resultString)
+	fmt.Printf("Part 1: %d\n", result)
 	executionTime := float32(time.Since(startTime).Milliseconds()) / float32(1000)
 	fmt.Printf("Completed Part 1 in %f seconds\n\n", executionTime)
+
+	startTime = time.Now()
+	result = part2(availableTowels, desiredPatterns)
+	fmt.Printf("Part 2: %d\n", result)
+	executionTime = float32(time.Since(startTime).Milliseconds()) / float32(1000)
+	fmt.Printf("Completed Part 2 in %f seconds\n\n", executionTime)
 }
 
-func part1(availableTowels map[string]bool, desiredPatterns []string) any {
+func part1(availableTowels map[string]bool, desiredPatterns []string) int {
 
 	patternMemo := make(map[string]bool)
 
@@ -88,4 +93,70 @@ func part1(availableTowels map[string]bool, desiredPatterns []string) any {
 	}
 	return possibleCount
 
+}
+
+func part2(availableTowels map[string]bool, desiredPatterns []string) int {
+
+	letters := []string{"w", "u", "b", "r", "g"}
+	singleColors := make(map[string]bool)
+	prefixPatternMap := make(map[string][]string)
+	cache := make(map[string]int)
+
+	for _, l1 := range letters {
+		for _, l2 := range letters {
+			key := l1 + l2
+			prefixPatternMap[key] = make([]string, 0)
+		}
+	}
+
+	for pattern := range availableTowels {
+		if len(pattern) == 1 {
+			singleColors[pattern] = true
+		} else {
+			prefix := pattern[0:2]
+			prefixPatternMap[prefix] = append(prefixPatternMap[prefix], pattern)
+		}
+	}
+
+	var combos func(target string) int
+	combos = func(target string) int {
+
+		if n, found := cache[target]; found {
+			return n
+		}
+
+		if len(target) == 1 {
+			if singleColors[target] {
+				return 1
+			} else {
+				return 0
+			}
+		}
+
+		count := 0
+		if singleColors[target[0:1]] {
+			count += combos(target[1:])
+		}
+		prefix := target[0:2]
+		for _, pattern := range prefixPatternMap[prefix] {
+			if strings.HasPrefix(target, pattern) {
+				remain := target[len(pattern):]
+				if len(remain) == 0 {
+					count++
+				} else {
+					count += combos(remain)
+				}
+			}
+		}
+		cache[target] = count
+		return count
+
+	}
+
+	total := 0
+	for _, target := range desiredPatterns {
+		total += combos(target)
+	}
+
+	return total
 }
