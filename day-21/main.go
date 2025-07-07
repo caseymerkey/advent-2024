307ab9e9baaed82333789f92a82e567a99ef4103
Casey Merkey <casey.merkey@gmail.com>
2025-07-07 12:08:50 -0400

    Part 2

--- day-21/main.go
+++ day-21/main.go
@@ -4,9 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-	"math"
 	"os"
-	"slices"
 	"strconv"
 	"strings"
 	"time"
@@ -30,10 +28,16 @@ func main() {
 	}
 
 	startTime := time.Now()
-	result := part1(codes)
+	result := evaluate(codes, 2)
 	fmt.Printf("Part 1: %d\n", result)
 	executionTime := float32(time.Since(startTime).Milliseconds()) / float32(1000)
 	fmt.Printf("Completed Part 1 in %f seconds\n\n", executionTime)
+
+	startTime = time.Now()
+	result = evaluate(codes, 25)
+	fmt.Printf("Part 2: %d\n", result)
+	executionTime = float32(time.Since(startTime).Milliseconds()) / float32(1000)
+	fmt.Printf("Completed Part 2 in %f seconds\n", executionTime)
 }
 
 type Coord struct {
@@ -72,59 +76,57 @@ var DirectionButtonMap = map[Coord]string{
 	{x: -1, y: 0}: "<",
 }
 
-func part1(codes []string) int {
+func evaluate(codes []string, iterations int) int {
 	total := 0
 
-	var expand func(directionButtons []string, level int) []string
-	expand = func(directionButtons []string, level int) []string {
-		if level == 0 {
-			return directionButtons
-		}
-
-		expanded := make([]string, 0)
-		start := DirectionPad["A"]
-
-		for _, dirButton := range directionButtons {
-			target := DirectionPad[dirButton]
-			buttonPath := legalPathFromAtoB(start, target, DirectionPad["*"])
-			expanded = append(expanded, buttonPath...)
-			start = target
-		}
-
-		return expand(expanded, level-1)
-	}
-
 	for _, code := range codes {
-		numPadKeys := strings.Split(code, "")
-		controlKeys := make([]string, 0)
+
+		cache := make(map[string]int)
 		start := NumberPad["A"]
-		for _, numKey := range numPadKeys {
-			target := NumberPad[numKey]
-			controlKeys = append(controlKeys, legalPathFromAtoB(start, target, NumberPad["*"])...)
-			start = target
+		for numberKey := range strings.SplitSeq(code, "") {
+			sequence := legalPathFromAtoB(start, NumberPad[numberKey], NumberPad["*"])
+			cache[sequence] = cache[sequence] + 1
+			start = NumberPad[numberKey]
 		}
-		controlKeys = expand(controlKeys, 2)
 
-		l, _ := strconv.Atoi(code[:len(code)-1])
-		total += len(controlKeys) * l
-
+		for range iterations {
+			newCache := make(map[string]int)
+			for topSeq := range cache {
+				factor := cache[topSeq]
+				start := DirectionPad["A"]
+				for directionKey := range strings.SplitSeq(topSeq, "") {
+					sequence := legalPathFromAtoB(start, DirectionPad[directionKey], DirectionPad["*"])
+					newCache[sequence] = newCache[sequence] + factor
+					start = DirectionPad[directionKey]
+				}
+			}
+			cache = newCache
+		}
+		length := 0
+		for sequence := range cache {
+			length += len(sequence) * cache[sequence]
+		}
+		val, _ := strconv.Atoi(code[:len(code)-1])
+		codeTotal := length * val
+		fmt.Printf("%s ==> %d * %d = %d\n", code, val, length, codeTotal)
+		total += codeTotal
 	}
 
 	return total
 }
 
-func legalPathFromAtoB(a, b, illegal Coord) []string {
-	path := make([]string, 0)
+func legalPathFromAtoB(a, b, illegal Coord) string {
+	var path strings.Builder
 
 	dx := b.x - a.x
 	dy := b.y - a.y
-	cx := []string{">"}
+	cx := ">"
 	if dx < 0 {
-		cx[0] = "<"
+		cx = "<"
 	}
-	cy := []string{"^"}
+	cy := "^"
 	if dy < 0 {
-		cy[0] = "v"
+		cy = "v"
 	}
 
 	// Using details posted here to decide on which order (left/right/up/down) to do first:
@@ -135,13 +137,20 @@ func legalPathFromAtoB(a, b, illegal Coord) []string {
 	// or we can't do the up/down first because we'd be moving into the illegal space
 	if (dx <= 0 && !(a.y == illegal.y && b.x == illegal.x)) || (dx > 0 && a.x == illegal.x) {
 		// do left/right first, then up/down
-		path = append(path, slices.Repeat(cx, int(math.Abs(float64(dx))))...)
-		path = append(path, slices.Repeat(cy, int(math.Abs(float64(dy))))...)
+		path.WriteString(strings.Repeat(cx, abs(dx)))
+		path.WriteString(strings.Repeat(cy, abs(dy)))
 	} else {
-		path = append(path, slices.Repeat(cy, int(math.Abs(float64(dy))))...)
-		path = append(path, slices.Repeat(cx, int(math.Abs(float64(dx))))...)
+		path.WriteString(strings.Repeat(cy, abs(dy)))
+		path.WriteString(strings.Repeat(cx, abs(dx)))
 	}
 
-	path = append(path, "A")
-	return path
+	path.WriteString("A")
+	return path.String()
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
